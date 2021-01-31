fd8d82cba1b7358e58492dcd0fcf0a2c92e04010
Jose <jmejiamu@gmail.com>
2021-01-31 00:40:19 -0800

    'styled-error-message'

--- hw2/part2/js/script.js
+++ hw2/part2/js/script.js
@@ -5,18 +5,28 @@ const sum = () => {
     let num2 = document.getElementById("num2").value;
     let num3 = document.getElementById("num3").value;
 
-    let total = parseInt(num1) + parseInt(num2) + parseInt(num3);
-    let product = parseInt(num1) * parseInt(num2) * parseInt(num3);
+    let n1 = parseInt(num1);
+    let n2 = parseInt(num2);
+    let n3 = parseInt(num3);
+
+    let total = n1 + n2 + n3;
+    let product = n1 * n2 * n3;
 
     if (isNaN(total)) {
-        document.getElementById("error").innerHTML = "Error"
+        document.getElementById("error").innerHTML = `
+        <p class="alert-error">Error you must enter valid values</p>`
+        document.getElementById("result").innerHTML = ""
     } else {
 
         document.getElementById("result").innerHTML = `
-        <p>Sum: ${total}
-            Product: ${product}
-            Average: ${total / 3}
+        <p>
+            Sum: ${total}<br>
+            Product: ${product}<br>
+            Average: ${total / 3}<br>
+            Max value: ${Math.max(n1, n2, n3)}<br>
+            Min value: ${Math.min(n1, n2, n3)}<br>
         </p>`;
+        document.getElementById("error").innerHTML = ""
     }
 
 }
