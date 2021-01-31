0a421bbb6638ab922c473d3e34c3a6a3162a4f95
Jose <jmejiamu@gmail.com>
2021-01-30 16:53:30 -0800

    'style-back-navigation'

--- hw2/part1/js/script.js
+++ hw2/part1/js/script.js
@@ -8,7 +8,7 @@ let number2 = 89;
 
 
 document.write(`
-        <p style= "text-align:center; margin-top:100px; font-size:20px;"> <strong>Output: </strong>
+        <p style= "text-align:center; margin-top:80px; font-size:20px;"> <strong>Output: </strong>
         <spam style = "color : red; font-family: Times New Roman;">${letter} </spam> 
         <spam style = "color : blue; font-family: Arial;"> ${number1}</spam> 
         <spam style = "color : green; font-family: Impact;font-style: italic; ">${number2}</spam> 
