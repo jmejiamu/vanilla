bdabf93f4e8033a6a997ea278d05f57726385087
Jose <jmejiamu@gmail.com>
2021-02-13 01:03:06 -0800

    adding logic to the input

--- hw3/part1/js/script.js
+++ hw3/part1/js/script.js
@@ -12,12 +12,43 @@ const studentGrades = () => {
     let finalExam = parseInt(final);
     let p = parseInt(participation);
 
-    // getting total average score
-    let finalAverage = (.5 * hwAvg) + (.2 * midExam) + (.2 * finalExam) + (.1 * p);
+    // Checking if the users' input is 0 - 100
+    if ((hwAvg <= 100 && hwAvg >= 0) && (midTerm <= 100 && midTerm >= 0) && (finalExam <= 100 && finalExam >= 0) && (p <= 100 && p >= 0)) {
 
-    if (Number.isNaN(finalAverage)) {
-        document.getElementById("error").innerHTML = `
-        <p class="alert-error" >Enter valid values, make sure all fields are filled with any number.</p>`
+        // getting total average score
+        let finalAverage = (.5 * hwAvg) + (.2 * midExam) + (.2 * finalExam) + (.1 * p);
+
+        average(finalAverage)
+
+    } else {
+        handleError();
     }
 
+}
+
+// This function handle the total average grades ex. 90 - 100 A   
+const average = (total) => {
+    if (Number.isNaN(total)) {
+        handleError();
+    } else {
+
+        document.getElementById("result").innerHTML = `
+        <p>
+        <strong>
+        Average Grade: ${parseInt(total)}<br>
+        </p>`;
+        document.getElementById("error").innerHTML = ""
+    }
+}
+
+const handleError = () => {
+    document.getElementById("error").innerHTML = `
+        <p class="alert-error" >Enter valid values 0 -100 and make sure all fields are filled with any number.</p>`
+    document.getElementById("result").innerHTML = ""
+
+}
+
+const resetResult = () => {
+    document.getElementById("result").innerHTML = ""
+    document.getElementById("error").innerHTML = ""
 }
