b425e6e356ba298f7c129d3dfad581391cd1207e
Jose <jmejiamu@gmail.com>
2021-02-14 18:05:34 -0800

    added error handler and check if it is not a number

--- hw3/part2/js/script.js
+++ hw3/part2/js/script.js
@@ -5,5 +5,36 @@ const itemInput = () => {
     let four = document.getElementById("item4").value;
 
     let ione = parseInt(one);
-    document.getElementById("item-one").value = ione;
+    let itwo = parseInt(two);
+    let ithree = parseInt(three);
+    let ifour = parseInt(four);
+
+
+    checkIsNotNumber(ione, itwo, ithree, ifour);
+    // let test = document.getElementById("item-one").value = ione;
+    // let test2 = document.getElementById("item-two").value = itwo;
+    // document.getElementById("item-three").value = ithree;
+    // document.getElementById("item-four").value = ifour;
+
+    // console.log(test + test2);
+
+}
+
+const checkIsNotNumber = (one, two, three, four) => {
+    let check = one + two + three + four;
+    if (Number.isNaN(check)) {
+        handleError();
+    } else {
+        let test = document.getElementById("item-one").value = one;
+        let test2 = document.getElementById("item-two").value = two;
+        document.getElementById("item-three").value = three;
+        document.getElementById("item-four").value = four;
+    }
+}
+
+const handleError = () => {
+    document.getElementById("error").innerHTML = `
+    <p class="alert-error" >Enter valid values 0 - 100 and make sure all fields are filled with any number.</p>`;
+    document.getElementById("result").value = ""
+
 }
