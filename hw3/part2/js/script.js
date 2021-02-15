85d18248fecff886d9cc3ebd921ae91c88abd608
Jose <jmejiamu@gmail.com>
2021-02-14 17:09:48 -0800

    added ids for each item

---
+++ hw3/part2/js/script.js
@@ -0,0 +1,6 @@
+const itemInput = () => {
+    let one = document.getElementById("item-one").value;
+    let two = document.getElementById("item-two").value;
+    let three = document.getElementById("item-three").value;
+    let four = document.getElementById("item-four").value;
+}
