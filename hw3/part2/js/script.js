3d9124be4bd9ea35d743ffb966eccab44c76406e
Jose <jmejiamu@gmail.com>
2021-02-14 17:27:17 -0800

    remove placeholder and add readonly property

--- hw3/part2/js/script.js
+++ hw3/part2/js/script.js
@@ -1,6 +1,9 @@
 const itemInput = () => {
-    let one = document.getElementById("item-one").value;
-    let two = document.getElementById("item-two").value;
-    let three = document.getElementById("item-three").value;
-    let four = document.getElementById("item-four").value;
+    let one = document.getElementById("item1").value;
+    let two = document.getElementById("item2").value;
+    let three = document.getElementById("item3").value;
+    let four = document.getElementById("item4").value;
+
+    let ione = parseInt(one);
+    document.getElementById("item-one").value = ione;
 }
