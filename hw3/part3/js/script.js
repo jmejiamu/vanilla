3897aac04bf78cea06bc25cdbdc03f9bf5d2cbc6
Jose <jmejiamu@gmail.com>
2021-02-15 20:47:53 -0800

    added handle error

--- hw3/part3/js/script.js
+++ hw3/part3/js/script.js
@@ -1,9 +1,18 @@
 const celciusToFarenheit = () => {
     let userInput = document.getElementById("user-input").value;
 
-    let conversion = (9 / 5 * userInput) + 32;
+    let input = parseInt(userInput)
 
-    document.getElementById("conversion").value = conversion;
+    if (Number.isNaN(input)) {
+        handleError();
+    } else {
+
+        let conversion = (9 / 5 * userInput) + 32;
+
+        document.getElementById("conversion").value = (`${userInput}C is ${parseInt(conversion)}F`);
+        document.getElementById("error").innerHTML = ""
+    }
+
 }
 
 const farenheitToCelcius = () => {
@@ -11,5 +20,12 @@ const farenheitToCelcius = () => {
 
     let conversion = 5 / 9 * (userInput - 32);
 
-    document.getElementById("conversion").value = conversion;
+    document.getElementById("conversion").value = `${userInput}F is ${parseInt(conversion)}C`;
+}
+
+
+const handleError = () => {
+    document.getElementById("error").innerHTML = `
+    <p class="alert-error" >Enter valid values 0 - 100 and make sure all fields are filled with any number.</p>`;
+    document.getElementById("conversion").value = ""
 }
