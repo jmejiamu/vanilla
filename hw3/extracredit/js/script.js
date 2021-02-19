8ed90763fa6a4450c0b7a47e553229b0617b961b
Jose <jmejiamu@gmail.com>
2021-02-18 19:19:19 -0800

    added random numbers

---
+++ hw3/extracredit/js/script.js
@@ -0,0 +1,9 @@
+
+let num1 = Math.floor((Math.random() * 10) + 1);
+document.getElementById("num1").innerHTML = num1;
+
+let num2 = Math.floor((Math.random() * 10) + 1);
+document.getElementById("num2").innerHTML = num2;
+
+let result = num1 * num2;
+console.log(result);
