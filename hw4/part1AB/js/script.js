08a9f6a2946cf018f9e14969be444a8fd10d5d7c
Jose <jmejiamu@gmail.com>
2021-03-07 22:08:07 -0800

    showing result for sum and multiplication

--- hw4/part1AB/js/script.js
+++ hw4/part1AB/js/script.js
@@ -5,8 +5,8 @@ const showResults = () => {
     for (let i = 5; i <= 25; i += 4) {
         sum += i;
         mul *= i;
+
     }
     document.getElementById("sum").innerHTML = `<h1>The sum of 5 + 9 + 13 + 17 + 21 + 25 is : ${sum} </h1>`
-    console.log(sum);
-    console.log(mul);
+    document.getElementById("mul").innerHTML = `<h1>The multiplication of 5 * 9 * 13 * 17 * 21 * 25 is : ${mul.toLocaleString()} </h1>`
 }
