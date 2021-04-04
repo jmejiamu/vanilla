90cb8bc8bf67020f854fcb5171f39aca156c7fc4
Jose <jmejiamu@gmail.com>
2021-04-03 22:11:50 -0700

    fixed logic bug for validation

--- hw5/part1/js/script.js
+++ hw5/part1/js/script.js
@@ -3,16 +3,37 @@ const checkForm = () => {
     let optionOne = document.getElementById("option1").checked;
     let optionTwo = document.getElementById("option2").checked;
     let optionThree = document.getElementById("option3").checked;
+    console.log(optionOne);
 
     let male = document.getElementById("male").checked;
     let female = document.getElementById("female").checked;
     let other = document.getElementById("other").checked;
 
-    if (!optionOne && !optionThree && !optionTwo) {
-        alert("Chose one of the option")
-    }
+    if ((optionOne || optionThree || optionTwo) && (male || female || other)) {
+        submitSuccess()
+        document.getElementById("errorCareer").innerHTML = ""
+        document.getElementById("error").innerHTML = ""
+    } else {
+        handleError();
 
-    if (!male && !female && !other) {
-        alert("chose one option RRRRR")
     }
+    // if (!male && !female && !other) {
+    //     handleError();
+    // } else {
+
+    //     document.getElementById("error").innerHTML = ""
+    // }
+
+}
+
+const handleError = () => {
+    document.getElementById("error").innerHTML = `
+    <p class="alert-error">Choose an option</p> `;
+    document.getElementById("errorCareer").innerHTML = `
+    <p class="alert-error">Choose an option  check</p> `;
+}
+
+const submitSuccess = () => {
+    document.getElementById("success").innerHTML = `
+    <p class="alert-error">data submitted successful</p> `;
 }
