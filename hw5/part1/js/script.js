c2ce83169dba3044346bf13fa94a5dccc0a83763
Jose <jmejiamu@gmail.com>
2021-04-04 00:37:48 -0700

    fix logic error bug

--- hw5/part1/js/script.js
+++ hw5/part1/js/script.js
@@ -1,6 +1,7 @@
 // Validating the checkBox
 const checkForm = () => {
 
+
     let userInput = document.getElementById("user-name").value;
     // Check box
     let optionOne = document.getElementById("option1").checked;
@@ -16,12 +17,8 @@ const checkForm = () => {
 
     if ((optionOne || optionThree || optionTwo)
         && (male || female || other)
-        && (dropDown !== 0) && (userInput !== "")) {
+        && (dropDown.selectedIndex !== 0) && (userInput !== "")) {
         submitSuccess()
-        document.getElementById("errorCareer").innerHTML = ""
-        document.getElementById("error").innerHTML = ""
-        document.getElementById("errorDrop").innerHTML = ""
-        document.getElementById("errorInput").innerHTML = ""
     } else {
         handleError();
 
@@ -30,18 +27,35 @@ const checkForm = () => {
 
 }
 
+
+
 const handleError = () => {
     document.getElementById("error").innerHTML = `
-    <p class="alert-error">Choose an option</p> `;
-    document.getElementById("errorCareer").innerHTML = `
-    <p class="alert-error">Choose an option  check</p> `;
-    document.getElementById("errorDrop").innerHTML = `
-    <p class="alert-error">Choose an option  errorDrop</p> `;
-    document.getElementById("errorInput").innerHTML = `
-    <p class="alert-error">Choose an option  errorInput</p> `;
+    <p class="alert-error">Complete all the  fields</p> `;
+    document.getElementById("success").innerHTML = "";
+
 }
 
 const submitSuccess = () => {
     document.getElementById("success").innerHTML = `
-    <p class="alert-error">data submitted successful</p> `;
+    <p class="su">data submitted successful</p> `;
+
+    //Clean errors
+    document.getElementById("error").innerHTML = ""
+
+
+    //Input
+    document.getElementById("user-name").value = "";
+
+    // radio
+    document.getElementById("male").checked = false;
+    document.getElementById("female").checked = false;
+    document.getElementById("other").checked = false;
+
+    // Check box
+    document.getElementById("option1").checked = false;
+    document.getElementById("option2").checked = false;
+    document.getElementById("option3").checked = false;
+
+    document.getElementById("years").selectedIndex = 0;
 }
