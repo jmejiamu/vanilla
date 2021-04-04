001bd1bb1529d63259b6d4f55a257eeae53dc331
Jose <jmejiamu@gmail.com>
2021-04-03 18:15:36 -0700

    added radio button validation

--- hw5/part1/js/script.js
+++ hw5/part1/js/script.js
@@ -4,7 +4,15 @@ const checkForm = () => {
     let optionTwo = document.getElementById("option2").checked;
     let optionThree = document.getElementById("option3").checked;
 
+    let male = document.getElementById("male").checked;
+    let female = document.getElementById("female").checked;
+    let other = document.getElementById("other").checked;
+
     if (!optionOne && !optionThree && !optionTwo) {
         alert("Chose one of the option")
+    }
+
+    if (!male && !female && !other) {
+        alert("chose one option RRRRR")
     }
 }
