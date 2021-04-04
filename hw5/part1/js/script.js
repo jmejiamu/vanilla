a389e170efd54a1324358c9fa680be079a208bce
Jose <jmejiamu@gmail.com>
2021-04-03 22:45:58 -0700

    added drop down logic

--- hw5/part1/js/script.js
+++ hw5/part1/js/script.js
@@ -1,18 +1,23 @@
 // Validating the checkBox
 const checkForm = () => {
+    // Check box
     let optionOne = document.getElementById("option1").checked;
     let optionTwo = document.getElementById("option2").checked;
     let optionThree = document.getElementById("option3").checked;
-    console.log(optionOne);
 
+    // Radios
     let male = document.getElementById("male").checked;
     let female = document.getElementById("female").checked;
     let other = document.getElementById("other").checked;
 
-    if ((optionOne || optionThree || optionTwo) && (male || female || other)) {
+    let dropDown = document.getElementById("years");
+
+    if ((optionOne || optionThree || optionTwo)
+        && (male || female || other) && (dropDown !== 0)) {
         submitSuccess()
         document.getElementById("errorCareer").innerHTML = ""
         document.getElementById("error").innerHTML = ""
+        document.getElementById("errorDrop").innerHTML = ""
     } else {
         handleError();
 
@@ -26,6 +31,8 @@ const handleError = () => {
     <p class="alert-error">Choose an option</p> `;
     document.getElementById("errorCareer").innerHTML = `
     <p class="alert-error">Choose an option  check</p> `;
+    document.getElementById("errorDrop").innerHTML = `
+    <p class="alert-error">Choose an option  errorDrop</p> `;
 }
 
 const submitSuccess = () => {
