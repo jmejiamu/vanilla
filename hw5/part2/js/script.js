6bb5ff12fd11f1029b635fb268c785fb5b7ba4c5
Jose <jmejiamu@gmail.com>
2021-04-06 16:34:30 -0700

    added second form for using onchange event

--- hw5/part2/js/script.js
+++ hw5/part2/js/script.js
@@ -1,4 +1,23 @@
 
 const onClickEventHandler = () => {
+    let dropDown = document.getElementById('destination').value;
 
+    if (dropDown !== "none") {
+        window.open(dropDown);
+        success()
+    } else {
+        handleError();
+    }
+
+}
+
+
+const handleError = () => {
+    document.getElementById("error").innerHTML = `
+    <p class="alert-error">Select an option </p> `;
+
+}
+
+const success = () => {
+    document.getElementById("error").innerHTML = "";
 }
