c481c8412a230cf144d916f469e57c4ae3b7aed3
Jose <jmejiamu@gmail.com>
2021-04-12 09:51:41 -0700

    added script for tables

--- hw5/part3/js/script.js
+++ hw5/part3/js/script.js
@@ -33,21 +33,39 @@ createTable([
 ]);
 
 function myFunction() {
-    var input, filter, table, tr, td, i, txtValue;
+    var input, filter, table, tr, td, td2, i, txtValue;
     input = document.getElementById("myInput");
     filter = input.value.toUpperCase();
     table = document.getElementById("myTable");
     tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
+    for (i = 1; i < tr.length; i++) {
         td = tr[i].getElementsByTagName("td")[0];
+        // td2 = tr[i].getElementsByTagName("td")[1];
         if (td) {
             txtValue = td.textContent || td.innerText;
+
             if (txtValue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
+                document.getElementById("error").innerHTML = "";
             } else {
                 tr[i].style.display = "none";
+                document.getElementById("error").innerHTML = `
+                <p class="alert-error">Select an option </p> `;
             }
         }
+
+        // if (td2) {
+        //     txtValue = td2.textContent || td2.innerText;
+        //     console.log(txtValue);
+        //     if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        //         tr[i].style.display = "";
+        //         document.getElementById("error").innerHTML = "";
+        //     } else {
+        //         tr[i].style.display = "none";
+        //         document.getElementById("error").innerHTML = `
+        //         <p class="alert-error">Select an option </p> `;
+        //     }
+        // }
     }
 }
 
