b31fa8b63011145b21b0ab26c27719a0f37ddc52
Jose <jmejiamu@gmail.com>
2021-04-25 00:34:29 -0700

    added tenth position

--- hw6/part1/js/script.js
+++ hw6/part1/js/script.js
@@ -2,16 +2,47 @@
 
 const submitNumber = () => {
 
-  const DECIMAL = 4;
+    const number = document.getElementById('enter-number').value;
+    // const convertNumber = parseFloat(number);
 
-  const number = document.getElementById('enter-number').value;
-  // const convertNumber = parseFloat(number) * 1.0;
+    const checkDecimal = number.split(".");
 
-  if (number.length - (number.indexOf(".") + 1) < DECIMAL || Number.isInteger(number)) {
-    console.log("Please Enter a number with at least 4 decimal position");
-  }
+    // console.log(number[1].length);
 
+    if (checkDecimal[1] === undefined || checkDecimal[1].length < 4) {
+        errorHandle();
+    } else {
+        cleanErrorDecimal()
+        finalResult(number);
+    }
 
-  // document.getElementById('result').innerHTML = convertNumber;
+    // console.log(Number.isInteger(number));
+
+    // if (number.length - (number.indexOf(".") + 1) < DECIMAL) {
+    //     errorHandle();
+    // } else {
+    //     cleanErrorDecimal()
+    //     document.getElementById("result").innerHTML = number;
+    // }
+
+}
+
+const errorHandle = () => {
+    document.getElementById('error-decimal').innerHTML = "Please Enter a number with at least 4 decimal position";
+}
+
+const cleanErrorDecimal = () => {
+    document.getElementById('error-decimal').innerHTML = " ";
+
+}
+
+const finalResult = (number) => {
+    console.log(number);
+    let tenthPosition = parseFloat(number).toFixed(1);
+    document.getElementById('result').innerHTML = `
+        A) Nearest Integer: ${Math.round(number)}
+        B) Square Root & round it to an int: ${parseInt(Math.sqrt(number))}
+        C) Tenths Position: ${tenthPosition}
+    `
 
 }
