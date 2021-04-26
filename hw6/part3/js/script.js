10121ac2d65f850bf37205f74b77f229f503e74e
Jose <jmejiamu@gmail.com>
2021-04-26 02:26:02 -0700

    added three and four digit code

--- hw6/part3/js/script.js
+++ hw6/part3/js/script.js
@@ -5,13 +5,19 @@ const submitData = () => {
     const phoneNumber = document.getElementById('phone').value;
 
     let areCode = phoneNumber.split(")");
-    let tokenAreaCode = areCode[0].split('(')
+    let tokenAreaCode = areCode[0].split('(');
 
-    console.log(typeof token);
+    let threeDigit = phoneNumber.split("-");
+    let tokenThreeDigit = threeDigit[0].split(")");
+
+    let fourDigit = phoneNumber.split("-");
+    console.log(fourDigit);
 
     document.getElementById("area-code").value = `${tokenAreaCode[1]}`;
+    document.getElementById("three-code").value = `${tokenThreeDigit[1]}`;
+    document.getElementById("four-code").value = `${fourDigit[1]}`;
 
-    // console.log("Area code", tokenAreaCode[1]);
+
 
 
 }
