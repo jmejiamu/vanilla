80b34c54daeef3296eb2870f28aabcb3434519c6
Jose <jmejiamu@gmail.com>
2021-04-26 02:15:56 -0700

    added area code to the text input

--- hw6/part3/js/script.js
+++ hw6/part3/js/script.js
@@ -1,2 +1,17 @@
-
+// Formating phone number
 $('input[name="phone-number"]').mask('(000) 000-0000');
+
+const submitData = () => {
+    const phoneNumber = document.getElementById('phone').value;
+
+    let areCode = phoneNumber.split(")");
+    let tokenAreaCode = areCode[0].split('(')
+
+    console.log(typeof token);
+
+    document.getElementById("area-code").value = `${tokenAreaCode[1]}`;
+
+    // console.log("Area code", tokenAreaCode[1]);
+
+
+}
