74777f314ccefffee0a028bcee2cb9eef2f8c34f
Jose <jmejiamu@gmail.com>
2021-04-25 18:42:54 -0700

    added logic validation

--- hw6/part2/js/script.js
+++ hw6/part2/js/script.js
@@ -4,36 +4,34 @@ const submitTextAndSearch = () => {
 
     let userText = document.getElementById("enter-text").value;
     let userTextToLower = userText.toLowerCase();
+    console.log(">>>", userText.length);
 
     let charToSearch = document.getElementById("search-char").value;
     let charToSearchToLower = charToSearch.toLowerCase().charAt(0);
 
-    console.log(">>>>", charToSearch.length);
-    console.log(charToSearchToLower);
-
     if (userText.length === 0) {
-        document.getElementById("error-text").innerHTML = "Error text";
-
-    }
-
-    if (charToSearch.length === 0) {
-        document.getElementById("error-char").innerHTML = "Error char"
+        document.getElementById("error-text").innerHTML = "This cannot be empty";
+        document.getElementById("search-result").innerHTML = "";
     } else {
-        document.getElementById("error-text").innerHTML = "";
-        document.getElementById("error-char").innerHTML = "";
 
-        for (let i = 0; i < userTextToLower.length; i++) {
+        if (charToSearch.length === 0) {
+            document.getElementById("error-char").innerHTML = "This cannot be empty"
+            document.getElementById("search-result").innerHTML = ""
+        } else {
+            document.getElementById("error-text").innerHTML = "";
+            document.getElementById("error-char").innerHTML = "";
 
-            if (userTextToLower.charAt(i) === charToSearchToLower) {
-                countChar++;
+            for (let i = 0; i < userTextToLower.length; i++) {
+
+                if (userTextToLower.charAt(i) === charToSearchToLower) {
+                    countChar++;
+                }
             }
 
+            document.getElementById("search-result").innerHTML = `The letter <strong>${charToSearch}</strong> was found ${countChar}`;
         }
-
-        document.getElementById("search-result").innerHTML = `The letter ${charToSearch} was found ${countChar}`;
-
-
     }
+
 
 
     document.getElementById("original-text").innerHTML = userText;
