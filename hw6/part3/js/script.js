6c85dfc34e57a36c28e1f76dd01d390cd203e2c0
Jose <jmejiamu@gmail.com>
2021-04-26 15:20:57 -0700

    remove extra divs

--- hw6/part3/js/script.js
+++ hw6/part3/js/script.js
@@ -11,13 +11,9 @@ const submitData = () => {
     let tokenThreeDigit = threeDigit[0].split(")");
 
     let fourDigit = phoneNumber.split("-");
-    console.log(fourDigit);
 
     document.getElementById("area-code").value = `${tokenAreaCode[1]}`;
     document.getElementById("three-code").value = `${tokenThreeDigit[1]}`;
     document.getElementById("four-code").value = `${fourDigit[1]}`;
-
-
-
 
 }
